f5e14f694bee4447591bca34ca43b090c810bf42
Karun <karun@Karuns-Macbook-Air.local>
2016-10-25 23:56:23 +0530

    mlab uri

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: "mongodb://karun_jobexcel:mLabkarun_1994@ds023624.mlab.com:23624/jobexcel",
     options: {
-      user: '',
-      pass: ''
+      user: 'karun_jobexcel',
+      pass: 'mLabkarun_1994'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
