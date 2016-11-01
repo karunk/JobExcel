de9750bc40ff139a54e47617a3bc4d0f7f17f2bd
Karun <karun@Karuns-Macbook-Air.local>
2016-11-01 21:56:00 +0530

    title change

--- config/env/default.js
+++ config/env/default.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    title: 'JobExcel',
+    description: 'Fancy spreadsheet for job hunters',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
